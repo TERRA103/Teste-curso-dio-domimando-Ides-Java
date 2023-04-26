6b9a617d55f7b0072455cfffca878becd5e5b2d6
Andre <andre.terra8@gmail.com>
2023-04-26 16:02:48 -0300

    first Commit

---
+++ src/com/andre/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.andre;
+
+import com.andre.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Olá Mundo!"  + (a+b));*/
+	}
+
+}
